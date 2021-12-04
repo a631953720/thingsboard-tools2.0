8595dddadd5eda466f7cda0c417f44e634ba435e
kk <tairong_kang@innodisk.com>
2021-12-05 03:16:26 +0800

    feat: 提出部分邏輯跟刪除部分logger

--- src/component/user/getTenantToken.ts
+++ src/component/user/getTenantToken.ts
@@ -4,6 +4,7 @@ import WinstonLogger from '../../helpers/loggers';
 import checkTenantAdminOrTenantExist from './checkTenantAdminOrTenantExist';
 import createTenantAdmin from './createTenantAdmin';
 import createTenant from './createTenant';
+import checkAxiosError from '../../helpers/checkAxiosError';
 
 const loggers = new WinstonLogger({ type: 'User component' });
 const { tenantAdminName, tenantEmail } = env.TB_User;
@@ -14,11 +15,19 @@ const {
     loginTenant,
 } = TBUserConnecter;
 
+async function getSystemAdminToken() {
+    loggers.debug('Try to get admin token', 'Login system admin');
+    const response = await loginSystemAdminToken();
+    if (checkAxiosError(response)) {
+        return '';
+    }
+    return response.token;
+}
+
 export default async function tryGetTenantToken() {
-    loggers.debug('', 'Login system admin');
     let tenantAdminId = '';
     let tenantId = '';
-    const { token: adminToken } = await loginSystemAdminToken();
+    const adminToken = await getSystemAdminToken();
 
     // 1. 檢查Tenant admin是否存在並取得id
     loggers.debug({ tenantAdminName }, 'search tenantAdmin');
@@ -26,7 +35,6 @@ export default async function tryGetTenantToken() {
     const tenantAdminsInfo = await searchTenantAdmin(adminToken, tenantAdminName);
     if (checkTenantAdminOrTenantExist(tenantAdminsInfo)) {
         const firstTenantAdminId = tenantAdminsInfo.data[0].id.id;
-        loggers.debug('Tenant admin is exist, get first tenant admin id', 'Check Tenant admin');
         tenantAdminId = firstTenantAdminId;
     } else {
         loggers.warning('Tenant admin is not exist, try to create new tenant admin', 'Check Tenant admin');
@@ -35,7 +43,7 @@ export default async function tryGetTenantToken() {
     loggers.debug({ tenantAdminId }, 'Get tenant admin id success');
 
     // 2. 檢查Tenant是否存在並取得Tenant id
-    loggers.debug({ tenantEmail }, 'search tenant');
+    loggers.debug({ tenantEmail }, 'Search tenant');
     const searchTenantInfo = await searchTenant(
         adminToken,
         tenantAdminId,
@@ -44,7 +52,6 @@ export default async function tryGetTenantToken() {
 
     if (checkTenantAdminOrTenantExist(searchTenantInfo)) {
         const firstTenantId = searchTenantInfo.data[0].id.id;
-        loggers.debug('Tenant is exist, get first tenant id', 'Check Tenant');
         tenantId = firstTenantId;
     } else {
         loggers.warning('Tenant is not exist, try to create new tenant', 'Check Tenant');
@@ -55,6 +62,6 @@ export default async function tryGetTenantToken() {
     // 3. 取得Tenant token
     const tenantInfo = await loginTenant(adminToken, tenantId);
     const tenantToken = tenantInfo.token;
-    loggers.debug({ tenantToken }, 'Get tenant id success');
+    loggers.debug({ tenantToken }, 'Get tenant token success');
     return tenantToken;
 }
