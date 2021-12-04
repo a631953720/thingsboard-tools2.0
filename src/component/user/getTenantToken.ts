695af85e54c733a734ce496e736523bee3bc5317
kk <tairong_kang@innodisk.com>
2021-12-05 03:44:49 +0800

    refactor: 取出get tenant token複雜的邏輯

--- src/component/user/getTenantToken.ts
+++ src/component/user/getTenantToken.ts
@@ -1,10 +1,11 @@
 import env from '../../constants/env';
 import * as TBUserConnecter from '../../application/thingsboardConnecter/user';
-import WinstonLogger from '../../helpers/loggers';
+import WinstonLogger, { simpleMsg } from '../../helpers/loggers';
+import checkAxiosError from '../../helpers/checkAxiosError';
+import { SearchTenantAdminRes, SearchTenantRes } from '../../types/user';
 import checkTenantAdminOrTenantExist from './checkTenantAdminOrTenantExist';
 import createTenantAdmin from './createTenantAdmin';
 import createTenant from './createTenant';
-import checkAxiosError from '../../helpers/checkAxiosError';
 
 const loggers = new WinstonLogger({ type: 'User component' });
 const { tenantAdminName, tenantEmail } = env.TB_User;
@@ -24,44 +25,59 @@ async function getSystemAdminToken() {
     return response.token;
 }
 
-export default async function tryGetTenantToken() {
-    let tenantAdminId = '';
-    let tenantId = '';
-    const adminToken = await getSystemAdminToken();
-
-    // 1. 檢查Tenant admin是否存在並取得id
-    loggers.debug({ tenantAdminName }, 'search tenantAdmin');
-    // eslint-disable-next-line max-len
-    const tenantAdminsInfo = await searchTenantAdmin(adminToken, tenantAdminName);
+async function getTenantAdminId(token: string, tenantAdminsInfo:SearchTenantAdminRes) {
+    let tenantAdminId;
     if (checkTenantAdminOrTenantExist(tenantAdminsInfo)) {
         const firstTenantAdminId = tenantAdminsInfo.data[0].id.id;
         tenantAdminId = firstTenantAdminId;
     } else {
         loggers.warning('Tenant admin is not exist, try to create new tenant admin', 'Check Tenant admin');
-        tenantAdminId = await createTenantAdmin(adminToken);
+        tenantAdminId = await createTenantAdmin(token);
     }
     loggers.debug({ tenantAdminId }, 'Get tenant admin id success');
+    return tenantAdminId;
+}
+
+async function getTenantId(token: string, tenantInfo:SearchTenantRes, tenantAdminId: string) {
+    let tenantId;
+    if (checkTenantAdminOrTenantExist(tenantInfo)) {
+        const firstTenantId = tenantInfo.data[0].id.id;
+        tenantId = firstTenantId;
+    } else {
+        loggers.warning('Tenant is not exist, try to create new tenant', 'Check Tenant');
+        tenantId = await createTenant(token, tenantAdminId);
+    }
+    loggers.debug({ tenantId }, 'Get tenant id success');
+    return tenantId;
+}
+
+async function getTenantToken(adminToken: string, tenantId: string) {
+    const tenantInfo = await loginTenant(adminToken, tenantId);
+    if (checkAxiosError(tenantInfo)) return '';
+    const tenantToken = tenantInfo.token;
+    simpleMsg(`Get tenant token success: ${tenantToken}`);
+    return tenantToken;
+}
+
+export default async function tryGetTenantToken() {
+    const adminToken = await getSystemAdminToken();
+
+    // 1. 檢查Tenant admin是否存在並取得id
+    // eslint-disable-next-line max-len
+    const tenantAdminsInfo = await searchTenantAdmin(adminToken, tenantAdminName);
+    if (checkAxiosError(tenantAdminsInfo)) return '';
 
     // 2. 檢查Tenant是否存在並取得Tenant id
-    loggers.debug({ tenantEmail }, 'Search tenant');
+    const tenantAdminId = await getTenantAdminId(adminToken, tenantAdminsInfo);
     const searchTenantInfo = await searchTenant(
         adminToken,
         tenantAdminId,
         tenantEmail,
     );
-
-    if (checkTenantAdminOrTenantExist(searchTenantInfo)) {
-        const firstTenantId = searchTenantInfo.data[0].id.id;
-        tenantId = firstTenantId;
-    } else {
-        loggers.warning('Tenant is not exist, try to create new tenant', 'Check Tenant');
-        tenantId = await createTenant(adminToken, tenantAdminId);
-    }
-    loggers.debug({ tenantId }, 'Get tenant id success');
+    const tenantId = await getTenantId(adminToken, searchTenantInfo, tenantAdminId);
 
     // 3. 取得Tenant token
-    const tenantInfo = await loginTenant(adminToken, tenantId);
-    const tenantToken = tenantInfo.token;
-    loggers.debug({ tenantToken }, 'Get tenant token success');
+    const tenantToken = getTenantToken(adminToken, tenantId);
+
     return tenantToken;
 }
