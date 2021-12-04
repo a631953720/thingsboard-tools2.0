8f8dfce7623743c884f934c506938484d19070a0
kk <tairong_kang@innodisk.com>
2021-12-05 03:17:02 +0800

    feat: create API 新增API狀態檢查

--- src/component/user/createTenant.ts
+++ src/component/user/createTenant.ts
@@ -1,6 +1,7 @@
 import env from '../../constants/env';
 import * as TBUserConnecter from '../../application/thingsboardConnecter/user';
 import WinstonLogger from '../../helpers/loggers';
+import checkAxiosError from '../../helpers/checkAxiosError';
 
 const loggers = new WinstonLogger({ type: 'User component' });
 
@@ -16,12 +17,18 @@ const getTenantProfile = (tenantAdminId: string) => ({
 });
 
 export default async function createTenant(token: string, tenantAdminId: string) {
-    const newTenantAdminInfo = await TBUserConnecter.createTenant(
+    const newTenantInfo = await TBUserConnecter.createTenant(
         token,
         tenantAdminId,
         getTenantProfile(tenantAdminId),
     );
-    const newTenantId = newTenantAdminInfo.id.id;
-    loggers.debug({ newTenantAdminInfo }, 'Create new tenant');
+
+    if (checkAxiosError(newTenantInfo)) {
+        loggers.error('Create Tenant error', 'Create new tenant');
+        return '';
+    }
+
+    const newTenantId = newTenantInfo.id.id;
+    loggers.debug({ newTenantInfo }, 'Create new tenant');
     return newTenantId as string;
 }
--- src/component/user/createTenantAdmin.ts
+++ src/component/user/createTenantAdmin.ts
@@ -1,6 +1,7 @@
 import env from '../../constants/env';
 import * as TBUserConnecter from '../../application/thingsboardConnecter/user';
 import WinstonLogger from '../../helpers/loggers';
+import checkAxiosError from '../../helpers/checkAxiosError';
 
 const loggers = new WinstonLogger({ type: 'User component' });
 
@@ -9,7 +10,13 @@ export default async function createTenantAdmin(token: string) {
         token,
         { title: env.TB_User.tenantAdminName },
     );
+
+    if (checkAxiosError(newTenantAdminInfo)) {
+        loggers.error('Create Tenant error', 'Create new tenant');
+        return '';
+    }
+
     const newTenantAdminId = newTenantAdminInfo.id.id;
-    loggers.debug({ newTenantAdminInfo }, 'Create new tenant admin');
+    loggers.debug({ createTenantAdminName: newTenantAdminInfo.name }, 'Create new tenant admin');
     return newTenantAdminId as string;
 }
