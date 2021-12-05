70e780e706054fd8e07cfbf79649c8ad7fc0a37b
kk <tairong_kang@innodisk.com>
2021-12-05 20:45:28 +0800

    feat: 將所有可用功能export

--- src/component/user/index.ts
+++ src/component/user/index.ts
@@ -1,2 +1,7 @@
-// eslint-disable-next-line import/prefer-default-export
+export { default as getOrCreateNewTenantToGetToken } from './getOrCreateNewTenantToGetToken';
+export { default as createTenant } from './createTenant';
+export { default as createTenantAdmin } from './createTenantAdmin';
+export { default as getFirstTenantAdminId } from './getFirstTenantAdminId';
+export { default as getFirstTenantId } from './getFirstTenantId';
+export { default as getSystemAdminToken } from './getSystemAdminToken';
 export { default as getTenantToken } from './getTenantToken';
