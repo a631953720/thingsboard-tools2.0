86be6988654466f04ea952acfa39cdc3856dcb5c
kk <tairong_kang@innodisk.com>
2021-12-06 16:22:23 +0800

    refactor: 刪除沒有被共用的type

--- src/types/user.ts
+++ src/types/user.ts
@@ -3,41 +3,6 @@ export type SystemAdminProfileProps = {
     password: string
 }
 
-type TenantAdminEntity = {
-    id: {
-        id: string
-        entityType: string
-    },
-    name: string
-    title: string
-}
-
-export type SearchTenantAdminRes = {
-    status: number
-    data: Array<TenantAdminEntity>
-    hasNext: boolean
-    totalElements: number
-    totalPages: number
-}
-
-type TenantEntity = {
-    id: {
-        id: string
-        entityType: string
-    },
-    name: string
-    firstName: string
-    lastName: string
-}
-
-export type SearchTenantRes = {
-    status: number
-    data: Array<TenantEntity>
-    hasNext: boolean
-    totalElements: number
-    totalPages: number
-}
-
 export type TenantProfileProps = {
     authority: 'TENANT_ADMIN',
     email: string,
