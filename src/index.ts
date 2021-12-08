bc6d9e06e20997280553d4c4a8083293036fb91e
krong <tairong_kang@innodisk.com>
2021-12-08 13:57:50 +0800

    feat:開出基礎的API進入點

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import notFoundErrorHandler from './middleware/notFoundErrorHandler';
+import controllerErrorHandler from './middleware/controllerErrorHandler';
+import commonErrorHandler from './middleware/commonErrorHandler';
 import { SERVER } from './constants/env';
 import { simpleMsg } from './helpers/loggers';
 import { TBUser } from './router';
@@ -7,9 +10,16 @@ const app = express();
 const { port } = SERVER;
 
 app.use('/TB/user', TBUser);
-
-app.get('/hello', (req, res) => {
-    res.send('Hello, World!!');
+app.get('/hello', (req, res, next) => {
+    try {
+        throw new Error('1');
+    } catch (error) {
+        next(error);
+    }
 });
+
+app.use(notFoundErrorHandler);
+app.use(controllerErrorHandler);
+app.use(commonErrorHandler);
 
 app.listen(port, () => simpleMsg(`http server is running at port ${port}.`));
