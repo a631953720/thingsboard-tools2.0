fa2a211af7416920e73b2b2e2bd1807abdbc6956
kk <tairong_kang@innodisk.com>
2022-02-11 22:59:24 +0800

    feat: 開好預計要用的API

--- src/router/TBDevice.routes.ts
+++ src/router/TBDevice.routes.ts
@@ -12,7 +12,15 @@ router.use('/delete', deleteDevicesValidator);
 router.get('/test', (req, res) => {
     res.status(200).send('success');
 });
+
+router.get('/list', (req, res) => {
+    res.status(200).send('API 開發中');
+});
 router.post('/create', createTBDevices);
 router.delete('/delete', deleteTBDevices);
+
+router.post('/action', (req, res) => {
+    res.status(200).send('API 開發中');
+});
 
 export default router;
