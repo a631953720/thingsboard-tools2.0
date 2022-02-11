a9538f99122ab9ae2258510093a66e4232866f7c
kk <tairong_kang@innodisk.com>
2022-02-12 00:43:23 +0800

    feat: add timmer

--- src/helpers/utility.ts
+++ src/helpers/utility.ts
@@ -5,3 +5,12 @@ export function checkValueType(value: any, type: TypeList) {
     // eslint-disable-next-line valid-typeof
     return typeof value === type;
 }
+
+export function delay(time: number) {
+    // eslint-disable-next-line no-unused-vars
+    return new Promise((res, _rej) => {
+        setTimeout(() => {
+            res(1);
+        }, time * 1000);
+    });
+}
