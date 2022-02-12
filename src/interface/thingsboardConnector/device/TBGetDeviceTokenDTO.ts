309ddeba217540e3c7cea3ed815ecce246458a4a
kk <tairong_kang@innodisk.com>
2022-02-13 00:06:53 +0800

    feat: get device token when query & create device

---
+++ src/interface/thingsboardConnector/device/TBGetDeviceTokenDTO.ts
@@ -0,0 +1,21 @@
+import { GetDeviceTokenRes } from './TBDeviceInterface';
+
+export default class TBGetDeviceDTO implements GetDeviceTokenRes {
+    status: number;
+
+    createdTime: number;
+
+    credentialsId: string;
+
+    credentialsType: string;
+
+    errorMessage?: any;
+
+    constructor(data: any) {
+        this.status = data.status;
+        this.createdTime = data.createdTime;
+        this.credentialsId = data.credentialsId;
+        this.credentialsType = data.credentialsType;
+        this.errorMessage = data.errorMessage;
+    }
+}
