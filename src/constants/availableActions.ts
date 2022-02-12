77851d440e9ded091e9003ee3670a765358ecb2f
kk <tairong_kang@innodisk.com>
2022-02-12 17:04:00 +0800

    feat: add avialiableAction constant

---
+++ src/constants/availableActions.ts
@@ -0,0 +1,5 @@
+import { Actions } from '../interface/serviceRequest/device/setTBDeviceActionDTO';
+
+const availableActions: Actions[] = ['sendData', 'subscribeRPC'];
+
+export default availableActions;
