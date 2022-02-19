8c3bd2583bc46d7e23a7d0036278caf60ade1201
kk <a631953720@gmail.com>
2022-02-19 19:43:58 +0800

    feat: modify  mock data generater

--- src/library/mockData/generateMockData.ts
+++ src/library/mockData/generateMockData.ts
@@ -1,4 +1,4 @@
-import { checkValueType, checkArrayValueType, randomNum } from '../../helpers/utility';
+import { checkValueType, randomNum } from '../../helpers/utility';
 import WinstonLogger from '../../helpers/loggers';
 
 const loggers = new WinstonLogger({ type: 'Mock data entity' });
@@ -10,23 +10,9 @@ export default function generateMockData(obj: any) {
     const keys = Object.keys(obj);
     keys.forEach((key) => {
         if (checkValueType(obj[key], 'string')) newObj[key] = obj[key];
-        if (checkValueType(obj[key], 'number')) newObj[key] = obj[key];
-        // number array
-        else if (Array.isArray(obj[key])) {
-            const IsNumberArr = checkArrayValueType({
-                array: obj[key],
-                type: 'number',
-            });
-            if (obj[key][0] > obj[key][1]) throw new Error('array[0] > array[1]');
-            if (IsNumberArr && obj[key].length > 0 && obj[key].length <= 2) {
-                if (obj[key].length === 1)
-                    // eslint-disable-next-line prefer-destructuring
-                    newObj[key] = obj[key][0];
-                if (obj[key].length === 2) newObj[key] = randomNum(obj[key][1], obj[key][1], obj[key][0]);
-            } else {
-                throw new Error('set array error');
-            }
-        } else if (checkValueType(obj[key], 'object')) newObj[key] = generateMockData(obj[key]);
+        else if (checkValueType(obj[key], 'number')) newObj[key] = randomNum(obj[key]);
+        else if (Array.isArray(obj[key])) newObj[key] = obj[key].map((v: any) => randomNum(v));
+        else if (checkValueType(obj[key], 'object')) newObj[key] = generateMockData(obj[key]);
     });
     return newObj;
 }
